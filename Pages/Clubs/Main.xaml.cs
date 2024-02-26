e593dc1c7f2cc8482253e1d26e2bb9fc3cf54084
MatveiKylosov <matveikyl@mail.ru>
2024-02-26 18:16:15 +0500

    Разработанный модуль управления клубами. Kylosov

---
+++ Pages/Clubs/Main.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Practical_lesson_No._29.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Practical_lesson_No._29.Pages.Clubs
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public ClubsContext AllClub = new ClubsContext();
+        public Main()
+        {
+            InitializeComponent();
+            foreach (Models.Clubs Club in AllClub.Clubs)
+                Parent.Children.Add(new Elements.Item(Club, this));
+        }
+
+        private void AddClub(object sender, RoutedEventArgs e) => 
+            MainWindow.init.OpenPages(new Pages.Clubs.Add(this));
+    }
+}
