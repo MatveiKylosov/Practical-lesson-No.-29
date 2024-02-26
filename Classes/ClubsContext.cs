ec2fafd1d937bc61ef6689b14edd08c6819c63a8
MatveiKylosov <matveikyl@mail.ru>
2024-02-26 16:08:02 +0500

    Разработанные контексты для моделей. Kylosov

---
+++ Classes/ClubsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practical_lesson_No._29.Classes.Common;
+using Practical_lesson_No._29.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_lesson_No._29.Classes
+{
+    public class ClubsContext : DbContext
+    {
+        public DbSet<Clubs> Clubs { get; set; }
+        public ClubsContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+    }
+}
